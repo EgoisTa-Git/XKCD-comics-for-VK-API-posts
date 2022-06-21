bf014f3b7dafd5edcd00813d337fb07a9d42d3c2
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2022-06-22 00:44:42 +0300

    Create part of post_comic function

--- main.py
+++ main.py
@@ -31,10 +31,48 @@ def get_vk_groups(url, token, version):
     return response.json()
 
 
+def post_comic(url, group_id, token, version, image):
+    upload_server_payload = {
+        'access_token': token,
+        'v': version,
+        'group_id': group_id,
+    }
+    upload_server_response = requests.get(
+        f'{url}photos.getWallUploadServer',
+        params=upload_server_payload,
+    )
+    upload_server_response.raise_for_status()
+    response = upload_server_response.json()['response']
+    album_id, upload_url, user_id = response.values()
+    image_data = {
+        'photo': image,
+    }
+    upload_post = requests.post(upload_url, files=image_data)
+    upload_post.raise_for_status()
+    server, photo, hash_ = upload_post.json().values()
+    save_payload = {
+        'access_token': token,
+        'v': version,
+        'server': server,
+        'photo': photo,
+        'hash': hash_,
+    }
+    save_response = requests.get(
+        f'{url}photos.saveWallPhoto',
+        params=save_payload,
+    )
+    save_response.raise_for_status()
+    pprint(save_response.json())
+    # вызовите метод photos.saveWallPhoto с параметрами server, photo, hash
+    # wall.post в параметре attachments "photo" + {owner_id} + "_" + {photo_id}
+    pass
+
+
 if __name__ == '__main__':
     load_dotenv()
     vk_token = os.getenv('VK_ACCESS_TOKEN')
     vk_api_url = 'https://api.vk.com/method/'
+    vk_group = os.getenv('VK_GROUP_ID')
     xkcd_number = 353
     xkcd_url = f'https://xkcd.com/{xkcd_number}/info.0.json'
 
@@ -44,4 +82,5 @@ if __name__ == '__main__':
     with open(f'comic_{xkcd_number}.png', 'wb') as file:
         file.write(get_image(comic_image_url))
 
-    pprint(get_vk_groups(vk_api_url, vk_token, VK_API_VERSION))
+    with open(f'comic_{xkcd_number}.png', 'rb') as comic_image:
+        post_comic(vk_api_url, vk_group, vk_token, VK_API_VERSION, comic_image)
